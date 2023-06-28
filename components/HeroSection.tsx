f092117d4e8a241a000e342ace0aebce45f9e3b3
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-29 05:15:05 +0530

    * Demo .

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,29 +3,25 @@ import React from "react"
 import Image from "next/image"
 import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
+import {
+  AiOutlineGithub,
+  AiFillPhone,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+} from "react-icons/ai"
 
 const HeroSection = () => {
   return (
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.png"
-            alt=""
-            width={325}
-            height={325}
-            className=" shadow-2xl"
-          />
+          <Image src="/headshot.png" alt="" width={325} height={325} className=" shadow-2xl" />
         </div>
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Harsh!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I&#39;m a{" "}
-            <span className="font-semibold text-teal-600">
-              Software Engineer{" "}
-            </span>
-            based in Delhi, India. Working towards creating software that
-            makes life easier and more meaningful.
+            I&#39;m a <span className="font-semibold text-teal-600">Software Engineer </span>
+            based in Delhi, India. Working towards creating software that makes life easier and more meaningful.
           </p>
           <Link
             to="projects"
@@ -39,16 +35,28 @@ const HeroSection = () => {
             Projects
           </Link>
         </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <a href="https://github.com/Harsh244007" rel="noreferrer" target="_blank">
+            <AiOutlineGithub className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100" size={30} />
+          </a>
+
+          <a href="https://www.linkedin.com/in/harsh-patel244/" rel="noreferrer" target="_blank">
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+
+          <a href="mailto:patelharsh241999@gmail.com" rel="noreferrer" target="_blank">
+            <AiOutlineMail className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100" size={30} />
+          </a>
+          <a href="tel:+918141924047" rel="noreferrer" target="_blank">
+            <AiFillPhone className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100" size={30} />
+          </a>
+        </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
+        <Link to="about" activeClass="active" spy={true} smooth={true} offset={-100} duration={500}>
           <HiArrowDown size={35} className="animate-bounce" />
         </Link>
       </div>
