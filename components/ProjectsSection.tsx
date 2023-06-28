f142e3071a02e2364e4fe0c0486dc0747c010102
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-29 03:55:40 +0530

    * New commit5 .

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -44,13 +44,13 @@ const ProjectsSection = () => {
           return (
             <div key={idx}>
               <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                <div className="flex flex-col mb-8  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                   <div className=" md:w-1/2">
                     <Link href={project.link}>
                       <Image src={project.image} alt="" width={1000} height={1000} className="rounded-xl shadow-xl hover:opacity-70" />
                     </Link>
                   </div>
-                  <div className="mt-8 md:w-1/2">
+                  <div className="md:w-1/2">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">{project.description}</p>
                     <div className="flex flex-row align-bottom space-x-4">
