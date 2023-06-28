d3e50b3cefc4bcf7f874bcc0d92d2064fe52bb93
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-29 03:14:19 +0530

    * New commit .

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -2,17 +2,17 @@ import React from "react"
 import Image from "next/image"
 
 const skills = [
-  { skill: "HTML" },
-  { skill: "CSS" },
-  { skill: "JavaScript" },
-  { skill: "TypeScript" },
-  { skill: "Python" },
+  { skill: "AEM" },
   { skill: "React" },
   { skill: "Next.js" },
-  { skill: "Tailwind CSS" },
+  { skill: "Node JS" },
+  { skill: "Express JS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Algorithms" },
+  { skill: "HTML" },
+  { skill: "CSS" },
   { skill: "Git" },
-  { skill: "GitHub" },
-  { skill: "Jupyter Notebooks" },
 ]
 
 const AboutSection = () => {
@@ -26,38 +26,29 @@ const AboutSection = () => {
 
         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
           <div className="md:w-1/2 ">
-            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
-              Get to know me!
-            </h1>
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">Get to know me!</h1>
             <p>
-              Hi, my name is Hosna and I am a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
+              Hi, my name is Harsh and I am a <span className="font-bold">{"highly ambitious"}</span>,
               <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> software engineer
-              based in Los Angeles, CA.
+              <span className="font-bold">{" driven"}</span> software engineer based in Delhi, India.
             </p>
             <br />
             <p>
-              I graduated from California State University, Northridge in 2019
-              with a BS in Computer Engineering and have been working in the
-              field ever since.
+              Ever since my college days, I had a strong passion for technology. I used to explore the realm of mobile devices by rooting them and
+              even managed to hack the college's Wi-Fi network. 
+              
+              This enthusiasm and interest in software eventually led me to pursue a career in this
+              field.
             </p>
             <br />
             <p>
-              I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
+              I have a wide range of hobbies and passions that keep me busy. From reading, playing sports, traveling, to making YouTube videos, I am
+              always seeking new experiences and love to keep myself engaged and learning new things.
             </p>
             <br />
             <p>
-              I believe that you should{" "}
-              <span className="font-bold text-teal-500">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
+              I believe that you should <span className="font-bold text-teal-500">never stop growing</span> and that&#39;s what I strive to do, I have
+              a passion for technology and a desire to always push the limits of what is possible. I am excited to see where my career takes me and am
               always open to new opportunities. 🙂
             </p>
           </div>
@@ -66,22 +57,13 @@ const AboutSection = () => {
             <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
               {skills.map((item, idx) => {
                 return (
-                  <p
-                    key={idx}
-                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
-                  >
+                  <p key={idx} className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold">
                     {item.skill}
                   </p>
                 )
               })}
             </div>
-            <Image
-              src="/hero-image.png"
-              alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
-            />
+            {/* <Image src="/hero-image.png" alt="" width={325} height={325} className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0" /> */}
           </div>
         </div>
       </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Hosna!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Harsh!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
