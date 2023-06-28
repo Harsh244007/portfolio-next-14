5a27595d6b806a947d179071743dc082680d6b8e
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-29 03:41:09 +0530

    * New commit3 .

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
             alt=""
             width={325}
             height={325}
-            className="rounded-full shadow-2xl"
+            className=" shadow-2xl"
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
