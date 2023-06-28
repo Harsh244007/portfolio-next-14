c5ed1a96c065bb59a7c937a758d6d7e0faad3dcc
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-29 04:04:39 +0530

    * Demo .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © {new Date().getFullYear()}{"&nbsp"}
+          © {new Date().getFullYear()}{" - "}
           <a href="https://www.linkedin.com/in/harsh-patel244/" rel="noreferrer" target="_blank">
             <p className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"> Harsh Patel</p>{" "}
           </a>
