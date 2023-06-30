fc4e674b6d096e5daf38dde3f96ea3bbb789d1a0
Harsh Patel <Patelharsh241999@gmail.com>
2023-06-30 21:35:18 +0530

    Revert "* Commit."
    This reverts commit 04abf754c35636935e9cae60ac342a7ab6aa32b8.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,12 @@
 "use client" // this is a client component
 import React from "react"
 import { useState, useEffect } from "react"
-import { Link as LinkScroll } from "react-scroll/modules"
+import { Link } from "react-scroll/modules"
 import { usePathname } from "next/navigation"
 import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 import "styles/globals.css"
-import Link from "next/link"
 interface NavItem {
   label: string
   page: string
@@ -25,7 +24,7 @@ const NAV_ITEMS: Array<NavItem> = [
   {
     label: "Projects",
     page: "projects",
-  }
+  },
 ]
 
 export default function Navbar() {
@@ -47,15 +46,15 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", progressBarHandler)
   })
   return (
-    <header key="home" className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <LinkScroll to="home">
+            <Link to="home">
               <div className="container flex items-center space-x-2">
                 <h2 className="text-2xl font-bold">Harsh Patel</h2>
               </div>
-            </LinkScroll>
+            </Link>
             <div className="md:hidden">
               <button className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}>
                 {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
@@ -69,7 +68,7 @@ export default function Navbar() {
             <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
               {NAV_ITEMS.map((item, idx) => {
                 return (
-                  <LinkScroll
+                  <Link
                     key={idx}
                     to={item.page}
                     className={"block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"}
@@ -81,21 +80,9 @@ export default function Navbar() {
                     onClick={() => setNavbar(!navbar)}
                   >
                     {item.label}
-                  </LinkScroll>
+                  </Link>
                 )
               })}
-                 <Link
-                    href={"/resume"}
-                    className={"block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"}
-                    // activeClass="active"
-                    // spy={true}
-                    // smooth={true}
-                    // offset={-100}
-                    // duration={500}
-                    // onClick={() => setNavbar(!navbar)}
-                  >
-                    {"Resume"}
-                  </Link>
               {/* {currentTheme === "dark" ? (
                 <button onClick={() => setTheme("light")} className="bg-slate-100 p-2 rounded-xl">
                   <RiSunLine size={25} color="black" />
