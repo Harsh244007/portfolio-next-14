558e1f019179a3fcb8ff6b916ac37e91d5703c9e
Harsh Patel <Patelharsh241999@gmail.com>
2023-07-19 20:07:48 +0530

    * Added clarity .

--- app/head.tsx
+++ app/head.tsx
@@ -4,13 +4,6 @@ export default function Head() {
       <title>Harsh Patel | Portfolio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Portfolio | Harsh Patel" />
-      <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "i1s5ks229h");
-</script>
       <link
         rel="shortcut icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>"
