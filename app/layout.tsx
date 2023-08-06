5e7ac133e7b74ee1799c786a36ef1d9faa2eace7
Harsh Patel <Patelharsh241999@gmail.com>
2023-08-07 00:08:23 +0530

    * Removed TTS Engine .

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Footer />
-            <TextToSpeechPlayer/>
+            {/* <TextToSpeechPlayer/> */}
           </ThemeProvider>
         ) : (
           <div className="loader">
