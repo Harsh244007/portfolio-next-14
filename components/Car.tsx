7066b901a1a4deb41a3d05089b9c7364629f060c
Harsh Patel <Patelharsh241999@gmail.com>
2023-08-11 05:23:24 +0530

    Update Car.tsx

--- components/Car.tsx
+++ components/Car.tsx
@@ -10,6 +10,10 @@ interface IframeComponentProps {
 const CarComponent: React.FC<IframeComponentProps> = ({ url }) => {
   return (
     <section id="car" className="w-full h-screen flex gap-10 flex-col justify-center items-center">
+     <h1 className="text-center font-bold text-4xl">
+          No Div CAR (CSS Only)
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
       <iframe
         src={url}
         className="w-full h-full border-none"
