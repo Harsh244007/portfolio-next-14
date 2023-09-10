05b2cb6642509d414fae6e471f17398509ee3376
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-10 13:49:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,7 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
  <HeroSection />
-    <div className="iframe">
-      <SolarComponent url={iframeUrlSolar}/>
-    </div>
+  
     <div className="iframe">
       <TictactoeComponent url={iframeUrlTic} />
     </div>
