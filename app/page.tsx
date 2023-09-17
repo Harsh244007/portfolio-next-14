a73c604a501b3595b28f678f9f6919f0975fd15a
Harsh Patel <Patelharsh241999@gmail.com>
2023-09-17 21:39:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,8 @@ export default function Home() {
 
   return (
     <>
-  <video  className="bgVideo"   loop autoPlay   muted >
+      {/* // @ts-ignore */}
+  <video  autoplay className="bgVideo"   loop autoPlay   muted >
 <source src={"/NatureBG.webm"} type="video/webm" />
 <source src={"/NatureBG.mp4"} type="video/mp4" />
 
