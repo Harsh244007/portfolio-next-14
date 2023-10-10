74fe4999ca131994676c0b1b0fc61445e3427929
Harsh Patel <Patelharsh241999@gmail.com>
2023-10-11 00:55:30 +0530

    Update Comment.tsx

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -7,7 +7,7 @@ interface IframeComponentProps {
 
 const RecursiveCommentComponent: React.FC<IframeComponentProps> = ({ url }) => {
   return (
-<section id="tictactoe" className="w-full  my-4  flex flex-col justify-center items-center mt-10" tabIndex={1}>
+<section id="tictactoe" className="w-full  my-4  flex flex-col justify-center items-center mt-20" tabIndex={1}>
   <h1 className="text-center font-bold text-4xl fade fadeOut">
     Recursive Comment
     <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
