1dad24502f7e3af6ad36ccf461c548ad9c3b54e2
Harsh Patel <Patelharsh241999@gmail.com>
2023-11-21 02:30:21 +0530

    * Added Suspense for projejcts to fastenup loading project section.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,7 +7,6 @@ import { lazy, Suspense } from "react";
 import { NavigationType } from "@/types/types";
 const Navigation: NavigationType = lazy(() => import("@/app/components/nav"));
 export default async function ProjectsPage() {
-  
   // await new Promise((resolve) => setTimeout(resolve, 20000));
   return (
     <div className="relative pb-16">
@@ -17,21 +16,20 @@ export default async function ProjectsPage() {
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">Projects</h2>
-          <p className="mt-4 text-zinc-400">
-            Some of the projects are from personal work and some are on my own time.
-          </p>
+          <p className="mt-4 text-zinc-400">Some of the projects are from personal work and some are on my own time.</p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
-
-        <div className="grid grid-cols-1 gap-8 mx-auto">
-          <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {PROJECTJSON.map((project, index) => (
-              <Card key={index}>
-                <Article project={project} views={Math.round(Math.random() * (4980 - 120) + 120)} />
-              </Card>
-            ))}
+        <Suspense fallback={<Loading />}>
+          <div className="grid grid-cols-1 gap-8 mx-auto">
+            <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+              {PROJECTJSON.map((project, index) => (
+                <Card key={index}>
+                  <Article project={project} views={Math.round(Math.random() * (4980 - 120) + 120)} />
+                </Card>
+              ))}
+            </div>
           </div>
-        </div>
+        </Suspense>
         <div className="hidden w-full h-px md:block bg-zinc-800" />
       </div>
     </div>
