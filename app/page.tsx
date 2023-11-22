761b19942ad6b36d30e2fce559c7f4838ce512c4
Harsh Patel <Patelharsh241999@gmail.com>
2023-11-22 19:55:06 +0530

    * Added 3D Island and WEBRTC Project.

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ const navigation = [
   { name: "Projects", href: "/projects", target: "_self" },
   { name: "Contact", href: "/contact", target: "_self" },
   { name: "Resume", href: "/Resume-Harsh-Patel.pdf", target: "_blank" },
+  {name:"3D Designs",href:"https://next-three-js-island.vercel.app/",target:"_blank"}
 ];
 
 const aboutMe = {
